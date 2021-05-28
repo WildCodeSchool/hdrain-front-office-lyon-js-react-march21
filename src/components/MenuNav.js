87829faea45c7e51277dd1d9f6afaee416748bd1
Edouard Misset <edouardmisset@gmail.com>
2021-05-28 19:08:35 +0200

    Sementic Navbar & some styling

--- src/components/MenuNav.js
+++ src/components/MenuNav.js
@@ -2,22 +2,34 @@ import { NavLink } from 'react-router-dom';
 
 export default function MenuNav() {
   return (
-    <div>
-      <NavLink exact to="/">
-        Home
-      </NavLink>
-      <NavLink exact to="/monitoring">
-        Monitoring
-      </NavLink>
-      <NavLink exact to="/history">
-        History
-      </NavLink>
-      <NavLink exact to="/monitoring/neuralNetwork">
-        Neurons Network
-      </NavLink>
-      <NavLink exact to="/monitoring/virtualMachine">
-        Virtual Engine
-      </NavLink>
-    </div>
+    <nav className="navBar">
+      <ul className="navList">
+        <li className="navItem">
+          <NavLink exact to="/">
+            Home
+          </NavLink>
+        </li>
+        <li className="navItem">
+          <NavLink exact to="/monitoring">
+            Monitoring
+          </NavLink>
+        </li>
+        <li className="navItem">
+          <NavLink exact to="/history">
+            History
+          </NavLink>
+        </li>
+        <li className="navItem">
+          <NavLink exact to="/monitoring/neuralNetwork">
+            Neural Network
+          </NavLink>
+        </li>
+        <li className="navItem">
+          <NavLink exact to="/monitoring/virtualMachine">
+            Virtual Engine
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 }
