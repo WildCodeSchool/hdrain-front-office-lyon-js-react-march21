169e63c94aaf0ff49f6fd6773838715804cf1957
Edouard Misset <edouardmisset@gmail.com>
2021-05-28 19:25:20 +0200

    Only style the links in the page and not those in the navbar

--- src/components/MenuNav.js
+++ src/components/MenuNav.js
@@ -5,27 +5,27 @@ export default function MenuNav() {
     <nav className="navBar">
       <ul className="navList">
         <li className="navItem">
-          <NavLink exact to="/">
+          <NavLink className="navLink" exact to="/">
             Home
           </NavLink>
         </li>
         <li className="navItem">
-          <NavLink exact to="/monitoring">
+          <NavLink className="navLink" exact to="/monitoring">
             Monitoring
           </NavLink>
         </li>
         <li className="navItem">
-          <NavLink exact to="/history">
+          <NavLink className="navLink" exact to="/history">
             History
           </NavLink>
         </li>
         <li className="navItem">
-          <NavLink exact to="/monitoring/neuralNetwork">
+          <NavLink className="navLink" exact to="/monitoring/neuralNetwork">
             Neural Network
           </NavLink>
         </li>
         <li className="navItem">
-          <NavLink exact to="/monitoring/virtualMachine">
+          <NavLink className="navLink" exact to="/monitoring/virtualMachine">
             Virtual Engine
           </NavLink>
         </li>
