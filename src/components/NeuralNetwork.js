d71c16d66e07f6fd7e1f7bf5af7555f08f358cf6
Edouard Misset <edouardmisset@gmail.com>
2021-05-28 18:20:21 +0200

    Used the LocationContext to retrieve the location and created a drop-down to select one from the list. Also disabled the button when no selection is made

--- src/components/NeuralNetwork.js
+++ src/components/NeuralNetwork.js
@@ -1,3 +1,38 @@
+import { useContext, useState } from 'react';
+import { LocationContext } from '../contexts/LocationContext';
+
 export default function NeuralNetwork() {
-  return <div>Neural Network</div>;
+  const { locationList, selectedLocation, setSelectedLocation } =
+    useContext(LocationContext);
+  const [isEnabled, setIsEnabled] = useState(true);
+
+  const handleLocationSelection = (event) => {
+    const locationValue = event.target.value;
+    setSelectedLocation(locationValue);
+    setIsEnabled(locationValue === 'None');
+  };
+
+  return (
+    <>
+      <h1>Neural Network</h1>
+      <h2>{selectedLocation}</h2>
+      <select name="location" id="location" onChange={handleLocationSelection}>
+        Location
+        <option key="None" value="None">
+          None
+        </option>
+        {locationList.map((location) => (
+          <option key={location} value={location}>
+            {location}
+          </option>
+        ))}
+      </select>
+      <button type="button" disabled={isEnabled}>
+        Neural Network
+      </button>
+      <button type="button" disabled={isEnabled}>
+        Assimilation
+      </button>
+    </>
+  );
 }
