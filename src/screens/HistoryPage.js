086abd18db5d46331198dd79a2b32db6ad5d6901
Doriane <berdalado@gmail.com>
2021-05-31 16:52:42 +0200

    add titles and btn without the navlink

--- src/screens/HistoryPage.js
+++ src/screens/HistoryPage.js
@@ -5,9 +5,12 @@ import { LocationContext } from '../contexts/LocationContext';
 export default function HistoryPage() {
   const { locationList, setSelectedLocation } = useContext(LocationContext);
   const [date, setDate] = useState(new Date());
+  const [isEnabled, setIsEnabled] = useState(true);
+
   const handleLocationSelection = (event) => {
     const locationValue = event.target.value;
     setSelectedLocation(locationValue);
+    setIsEnabled(locationValue === 'None');
   };
   return (
     <>
@@ -32,6 +35,26 @@ export default function HistoryPage() {
       </div>
       <p>Select a date</p>
       <DatePicker onChange={setDate} value={date} />
+      <div className="map">
+        <h3>Sensor map</h3>
+        <h3>Rain map</h3>
+        <br />
+        <button type="button" disabled={isEnabled}>
+          Get Log GLOBAL
+        </button>
+        <button type="button" disabled={isEnabled}>
+          Get Log Neural Network
+        </button>
+        <button type="button" disabled={isEnabled}>
+          Get Log Assimilation
+        </button>
+        <button type="button" disabled={isEnabled}>
+          Get parametres assimilation
+        </button>
+        <button type="button" disabled={isEnabled}>
+          Get assimilation costs
+        </button>
+      </div>
     </>
   );
 }
