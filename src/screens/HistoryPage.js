53c82e205586e09ae6607207d98bbc44854e6460
Doriane <berdalado@gmail.com>
2021-05-31 15:55:50 +0200

    select location part

--- src/screens/HistoryPage.js
+++ src/screens/HistoryPage.js
@@ -1,3 +1,37 @@
+import { useContext } from 'react';
+import { LocationContext } from '../contexts/LocationContext';
+
 export default function HistoryPage() {
-  return <div>History</div>;
+  const { locationList, setSelectedLocation } = useContext(LocationContext);
+
+  const handleLocationSelection = (event) => {
+    const locationValue = event.target.value;
+    setSelectedLocation(locationValue);
+  };
+  return (
+    <>
+      <h2>History</h2>
+      <div className="location">
+        <p>Select a location</p>
+        <select
+          name="location"
+          id="location"
+          onChange={handleLocationSelection}
+        >
+          Location
+          <option key="None" value="None">
+            None
+          </option>
+          {locationList.map((location) => (
+            <option key={location} value={location}>
+              {location}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="datepicker">
+        <p>Select a date</p>
+      </div>
+    </>
+  );
 }
