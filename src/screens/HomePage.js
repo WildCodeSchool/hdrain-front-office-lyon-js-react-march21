2cbc12138f1ff06c132ede0003940b6145a53347
Solene <solene.gerard@gmail.com>
2021-05-31 11:51:37 +0200

    created basic skeleton for homepage

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,3 +1,12 @@
 export default function HomePage() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <div>Choix du Site</div>
+      <h3>Détails du site choisi</h3>
+      <button type="button">Réseau de Neurones</button>
+      <button type="button">Assimilation</button>
+      <button type="button">Lien vers l'historique</button>
+    </div>
+  );
 }
