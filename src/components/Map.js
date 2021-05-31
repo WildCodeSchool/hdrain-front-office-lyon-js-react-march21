3d946e090a8b10ade2f64f201ff42ae75d1fd615
Edouard Misset <edouardmisset@gmail.com>
2021-05-31 11:16:10 +0200

    Created functions : to set the custom icon on the map, to set the view in the center

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,32 +1,113 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import blueSensorPath from '../assets/sensor-blue.png';
+import redSensorPath from '../assets/sensor-red.png';
+import greenSensorPath from '../assets/sensor-green.png';
+import locationPath from '../assets/sensor.png';
+
+// OBSOLETE ?
+/**
+ * @description This function convert one of three color's cname to a string to change the color of the default icon used in leaftlet
+ * @date 31/05/2021
+ * @param {*} color (string)
+ * @return {*} a css string attribute
+ */
+/* const convertColorToDeg = (color) => {
+  // In order to change the color of the pin (png) we use rotate it's hue
+  // green (240deg) = status OK & no rain, blue (0deg) = status OK & rain, red (140deg) = status NOK
+  if (color === 'red') return 'hue-rotate(140deg)';
+  if (color === 'green') return 'hue-rotate(240deg)';
+  if (color === 'blue') return 'hue-rotate(0deg)';
+  return 'grayscale(100%)';
+}; */
+
+const selectIcon = (type, color = 'none') => {
+  let iconPath = '';
+  if (type === 'location') {
+    iconPath = locationPath;
+  } else {
+    if (color === 'red') {
+      iconPath = redSensorPath;
+    }
+    if (color === 'green') {
+      iconPath = greenSensorPath;
+    }
+    if (color === 'blue') {
+      iconPath = blueSensorPath;
+    }
+  }
+
+  return new L.Icon({
+    iconUrl: iconPath,
+    iconRetinaUrl: iconPath,
+    // iconAnchor: null,
+    // popupAnchor: null,
+    // shadowUrl: null,
+    // shadowSize: null,
+    // shadowAnchor: null,
+    iconSize: new L.Point(50, 65),
+    className: 'leaflet-div-icon',
+  });
+};
+
+const findCenter = (pins) => {
+  // Returns a default centering value if no pins are present
+  if (pins.length < 1) return [50, 0];
+  // Returns the lat / lng of the first pin if only one pin
+  if (pins.length === 1) return [pins[0].lat, pins[0].lng];
+  // Else returns the barycenter of the n pins
+  return [
+    pins.reduce((acc, { lat }, _, array) => acc + lat / array.length, 0),
+    pins.reduce((acc, { lng }, _, array) => acc + lng / array.length, 0),
+  ];
+};
 
 export default function Map({
-  pin = {
-    lng: -4.033333,
-    lat: 5.316667,
-    name: 'Abidjan',
-    description: 'The weather is nice this time of year',
-  },
+  pins = [
+    {
+      lng: -4.033333,
+      lat: 5.316666,
+      name: 'Abidjan',
+      description: 'The weather is nice this time of year',
+      color: 'green',
+      type: 'sensor',
+    },
+    {
+      lng: -4.133333,
+      lat: 5.216666,
+      name: 'zef',
+      description: 'ezf',
+      color: 'blue',
+      type: 'location',
+    },
+  ],
 }) {
-  const { lng, lat, name, description } = pin;
-  const coordinates = [lat, lng];
   return (
     <MapContainer
-      style={{ height: '400px' }}
-      center={coordinates}
-      zoom={13}
+      center={findCenter(pins)}
+      zoom={pins.length <= 1 ? 13 : 11}
       scrollWheelZoom={false}
+      style={{
+        height: '400px',
+      }}
     >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={coordinates}>
-        <Popup>
-          <h4>{name}</h4>
-          <p>{description}</p>
-        </Popup>
-      </Marker>
+      {pins.length &&
+        pins.map(({ name, description, color, lat, lng, type }) => (
+          <Marker
+            key={name}
+            icon={selectIcon(type, color)}
+            position={[lat, lng]}
+          >
+            <Popup>
+              <h4>{name}</h4>
+              <p>{description}</p>
+            </Popup>
+          </Marker>
+        ))}
     </MapContainer>
   );
 }
