e29c08145bbf99d1596f4be3050f6cdbb5ffb6a5
Edouard Misset <edouardmisset@gmail.com>
2021-05-31 14:34:48 +0200

    Fixed the router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
 
 export default function App() {
   return (
-    <>
+    <Router>
       <Header />
       <Main />
       <Footer />
-    </>
+    </Router>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { LocationContextProvider } from '../contexts/LocationContext';
 import HistoryPage from '../screens/HistoryPage';
 import HomePage from '../screens/HomePage';
@@ -10,23 +10,21 @@ export default function Main() {
   return (
     <main>
       <LocationContextProvider>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/monitoring" component={MonitoringPage} />
-            <Route exact path="/history" component={HistoryPage} />
-            <Route
-              exact
-              path="/monitoring/neuralNetwork"
-              component={NeuralNetwork}
-            />
-            <Route
-              exact
-              path="/monitoring/virtualMachine"
-              component={VirtualMachine}
-            />
-          </Switch>
-        </Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/monitoring" component={MonitoringPage} />
+          <Route exact path="/history" component={HistoryPage} />
+          <Route
+            exact
+            path="/monitoring/neuralNetwork"
+            component={NeuralNetwork}
+          />
+          <Route
+            exact
+            path="/monitoring/virtualMachine"
+            component={VirtualMachine}
+          />
+        </Switch>
       </LocationContextProvider>
     </main>
   );
