6e3fd3a9202003304983a38def819e6eb17ce6bd
Solene <solene.gerard@gmail.com>
2021-06-01 08:35:40 +0200

    added space between buttons

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -8,12 +8,9 @@ export default function HomePage() {
     <>
       <h1>Home</h1>
       <LocationDropdown />
-      <NavLink to="/history">
-        <button className="history-btn" type="button">
-          Lien vers l'historique
-        </button>
-      </NavLink>
-
+      <br />
+      <NavLink to="/history">History Link</NavLink>
+      <br />
       <div>
         <img
           src="https://www.kindpng.com/picc/m/331-3317031_simple-silhouette-world-map-hd-png-download.png"
