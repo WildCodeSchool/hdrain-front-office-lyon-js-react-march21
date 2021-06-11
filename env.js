6f1f34eb95dff2147b264c3f67ad9a9ea2ef0cfa
Edouard Misset <edouardmisset@gmail.com>
2021-06-11 15:08:03 +0200

    Removed unnecessary dependency

--- env.js
+++ env.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 function getEnv(varibale) {
   const value = process.env[varibale];
   if (typeof value === 'undefined') {
-    console.warn(`Seems like the variable "${varibale}" is not set in the environment. 
+    window.console
+      .warn(`Seems like the variable "${varibale}" is not set in the environment. 
     Did you forget to execute "cp .env.sample .env" and adjust variables in the .env file to match your own environment ?`);
   }
   return value;
