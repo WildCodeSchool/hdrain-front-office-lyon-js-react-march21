68f8703a6dbfb50a3b308853dc01110cf78d01e4
Edouard Misset <edouardmisset@gmail.com>
2021-06-18 13:45:18 +0200

    Small changes to the headers

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import MenuNav from './MenuNav';
 export default function Header() {
   return (
     <header>
-      <h2>HD Rain</h2>
+      <h2 className="site-title">HD Rain Monitoring Tool</h2>
       <MenuNav />
     </header>
   );
