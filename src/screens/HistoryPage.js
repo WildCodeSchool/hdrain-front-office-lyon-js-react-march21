2ec3321dc868e88819342b0c88af439eb56a2fce
Edouard Misset <edouardmisset@gmail.com>
2021-06-20 18:32:56 +0200

    Styled calendar and download button

--- src/screens/HistoryPage.js
+++ src/screens/HistoryPage.js
@@ -41,7 +41,7 @@ export default function HistoryPage() {
         <h3>Rain map</h3>
         {isEnabled && <img src={rainMMap} alt="rainMap" />}
       </div>
-      <div>
+      <div className="download-links">
         <Link
           className="download"
           to={pathToLog}
