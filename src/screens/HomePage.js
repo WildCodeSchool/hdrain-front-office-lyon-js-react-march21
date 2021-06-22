97c1cda0afdd4a618361e78236a9a90a1853cb0e
Solene G <solene.gerard@gmail.com>
2021-06-22 23:02:48 +0200

    added MAP component to homepage

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -2,6 +2,7 @@ import { React, useContext } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { LocationContext } from '../contexts/LocationContext';
 import LocationDropdown from '../components/LocationDropDown';
+import Map from '../components/Map';
 
 export default function HomePage() {
   const { selectedLocationId, isParamsEmpty } = useContext(LocationContext);
@@ -42,10 +43,7 @@ export default function HomePage() {
       </NavLink>
       <br />
       <div>
-        <img
-          src="https://www.kindpng.com/picc/m/331-3317031_simple-silhouette-world-map-hd-png-download.png"
-          alt="worldmap"
-        />
+        <Map />
       </div>
     </>
   );
