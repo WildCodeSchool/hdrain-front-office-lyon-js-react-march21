31b116db8ea808b440a9ec3eb53fe9162f398a0c
Edouard Misset <edouardmisset@gmail.com>
2021-06-22 12:44:45 +0200

    Styled the login page

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -1,156 +1,97 @@
-import { useState } from 'react';
-import { useForm, Controller } from 'react-hook-form';
 import { useToasts } from 'react-toast-notifications';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Alert from '@material-ui/lab/Alert';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import { useForm } from 'react-hook-form';
 import API from '../APIClient';
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
-
 export default function LoginForm() {
-  const classes = useStyles();
-
   const { addToast } = useToasts();
-
-  const [loginError, setLoginError] = useState(null);
   const {
-    control,
+    register,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm({
     defaultValues: {
       username: '',
       password: '',
+      rememberMe: false,
     },
   });
 
-  const onSubmit = ({ username, password }) => {
+  window.console.log(watch('username'));
+
+  const onSubmit = (data) => {
+    window.console.log(data);
+    const { username, password } = data;
     API.post(`/auth/login`, { username, password })
       .then(() => {
-        addToast('Successfully logged in', { appearance: 'success' });
+        addToast('Successfully logged in', {
+          appearance: 'success',
+          autoDismiss: true,
+        });
       })
       .catch((err) => {
         window.console.error(err);
-        addToast('Wrong Credentials', { appearance: 'error' });
+        addToast('Wrong Credentials', {
+          appearance: 'error',
+          autoDismiss: true,
+        });
       });
-    setLoginError(null);
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form
-          className={classes.form}
-          noValidate
-          onSubmit={handleSubmit(onSubmit)}
+    <form className="form" onSubmit={handleSubmit(onSubmit)}>
+      <h2>Log In</h2>
+      <input
+        {...register('username', { required: true })}
+        autoComplete="username"
+        placeholder="Enter your email"
+        type="text"
+      />
+      {errors.username && (
+        <span
+          style={{
+            marginLeft: '1em',
+            marginTop: 0,
+            color: 'red',
+            fontSize: '.75rem',
+          }}
         >
-          {loginError && <Alert severity="error">{loginError}</Alert>}
-          <Controller
-            name="username"
-            control={control}
-            rules={{ required: { value: true, message: 'Required' } }}
-            render={({ field }) => (
-              <TextField
-                inputProps={field}
-                error={!!errors.username}
-                helperText={errors.username ? errors.username.message : ''}
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="username"
-                label="Username"
-                name="username"
-                autoComplete="username"
-                autoFocus
-              />
-            )}
-          />
-          <Controller
-            name="password"
-            control={control}
-            rules={{
-              minLength: {
-                value: 1,
-                message: 'should be at least 8 characters',
-              },
-            }}
-            render={({ field }) => (
-              <TextField
-                type="password"
-                inputProps={field}
-                error={!!errors.username}
-                helperText={errors.password ? errors.password.message : ''}
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                id="password"
-                autoComplete="current-password"
-              />
-            )}
-          />
-
-          <Controller
-            name="stayConnected"
-            control={control}
-            render={({ field }) => (
-              <FormControlLabel
-                control={
-                  <Checkbox {...field} name="checkedB" color="primary" />
-                }
-                label="Remember me"
-              />
-            )}
-          />
-
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Log In
-          </Button>
-        </form>
-      </div>
-    </Container>
+          This field is required
+        </span>
+      )}
+      <input
+        {...register('password', { required: true })}
+        placeholder="Password"
+        autoComplete="current-password"
+        type="password"
+        rules={{
+          minLength: {
+            value: 4,
+            message: 'should be at least 8 characters',
+          },
+        }}
+      />
+      {errors.password && (
+        <span
+          style={{
+            marginLeft: '1em',
+            marginTop: 0,
+            color: 'red',
+            fontSize: '.75rem',
+          }}
+        >
+          This field is required
+        </span>
+      )}
+      <span className="rememberMe">
+        <input
+          type="Checkbox"
+          {...register('rememberMe')}
+          label="Remember me"
+        />
+        Remember me
+      </span>
+      <input type="submit" value="Login" />
+    </form>
   );
 }
