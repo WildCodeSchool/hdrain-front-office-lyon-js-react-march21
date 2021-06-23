2974ab8cc815707b8fbd4ff7503899d406bab174
Solene G <solene.gerard@gmail.com>
2021-06-23 15:02:52 +0200

    adding logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import MenuNav from './MenuNav';
+import logo from '../assets/logo_HDRain.png';
 
 export default function Header() {
   return (
     <header>
+      <img className="nav-logo" src={logo} alt="HD RAIN" />
       <h2 className="site-title">HD Rain Monitoring Tool</h2>
       <MenuNav />
     </header>
