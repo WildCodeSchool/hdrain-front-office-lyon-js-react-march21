afc039eb7fa71d4a29f4aa72f00e66c47b60a58d
Edouard Misset <edouardmisset@gmail.com>
2021-06-23 15:42:06 +0200

    move some of the chart's options to another file and added a way to use our data

--- src/components/RainGraph.js
+++ src/components/RainGraph.js
@@ -1,156 +1,58 @@
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import graphOptions from './graphHelper';
 
-export default function RainGraph() {
-  const [chartOptions] = useState({
-    chart: {
-      zoomType: 'xy',
-    },
-    title: {
-      text: 'Rain Graph for Station XXX',
-      align: 'center',
-    },
-    subtitle: {
-      text: 'Station XXX mode NUV',
-      align: 'center',
-    },
-    xAxis: [
-      {
-        categories: [
-          'Jan',
-          'Feb',
-          'Mar',
-          'Apr',
-          'May',
-          'Jun',
-          'Jul',
-          'Aug',
-          'Sep',
-          'Oct',
-          'Nov',
-          'Dec',
-        ],
-        crosshair: true,
-      },
-    ],
-    yAxis: [
-      {
-        // Primary yAxis
-        gridLineWidth: 0,
-        title: {
-          text: 'Atténuation (dB)',
-          style: {
-            color: Highcharts.getOptions().colors[0],
-          },
-        },
-        labels: {
-          format: '{value} dB',
-          style: {
-            color: Highcharts.getOptions().colors[0],
-          },
-        },
-      },
-      {
-        // Secondary yAxis
-        labels: {
-          format: '{value}',
-          style: {
-            color: Highcharts.getOptions().colors[5],
-          },
-        },
-        title: {
-          text: 'Critère de Pluie / Non pluie',
-          style: {
-            color: Highcharts.getOptions().colors[5],
-          },
-        },
-        opposite: true,
-      },
-    ],
-    tooltip: {
-      shared: true,
-    },
-    legend: {
-      layout: 'vertical',
-      align: 'left',
-      x: 80,
-      verticalAlign: 'top',
-      y: 55,
-      floating: true,
-      backgroundColor:
-        Highcharts.defaultOptions.legend.backgroundColor || // theme
-        'rgba(255,255,255,0.25)',
-    },
-    series: [
-      {
-        name: 'Atténuation',
-        type: 'line',
-        yAxis: 0,
-        data: [
-          49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1,
-          95.6, 54.4,
-        ],
-        tooltip: {
-          valueSuffix: ' dB',
-        },
-      },
-      {
-        name: 'Critère de Pluie',
-        yAxis: 1,
-        type: 'line',
-        data: [
-          7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6,
-        ],
-        tooltip: {
-          valueSuffix: ' %',
-        },
-        color: Highcharts.getOptions().colors[5],
-      },
-    ],
-    responsive: {
-      rules: [
+export default function RainGraph({
+  inputDampening = [1, 2, 3, 4, 5, 6, 7, 8],
+  inputRain = [9, 8, 7, 6, 5, 4, 3, 2],
+  inputTime = [
+    new Date(2021, 5, 15).toLocaleDateString(),
+    new Date(2021, 5, 16).toLocaleDateString(),
+    new Date(2021, 5, 17).toLocaleDateString(),
+    new Date(2021, 5, 18).toLocaleDateString(),
+    new Date(2021, 5, 19).toLocaleDateString(),
+    new Date(2021, 5, 20).toLocaleDateString(),
+    new Date(2021, 5, 21).toLocaleDateString(),
+    new Date(2021, 5, 22).toLocaleDateString(),
+  ],
+}) {
+  const [chartOptions, setChartOptions] = useState(graphOptions);
+
+  const setChartData = (dampening, rain, time) =>
+    setChartOptions((previousOptions) => ({
+      ...previousOptions,
+      xAxis: [
         {
-          condition: {
-            maxWidth: 500,
-          },
-          chartOptions: {
-            legend: {
-              floating: false,
-              layout: 'horizontal',
-              align: 'center',
-              verticalAlign: 'bottom',
-              x: 0,
-              y: 0,
-            },
-            yAxis: [
-              {
-                labels: {
-                  align: 'right',
-                  x: 0,
-                  y: -6,
-                },
-                showLastLabel: false,
-              },
-              {
-                labels: {
-                  align: 'left',
-                  x: 0,
-                  y: -6,
-                },
-                showLastLabel: false,
-              },
-              {
-                visible: false,
-              },
-            ],
-          },
+          categories: time,
         },
       ],
-    },
-  });
+      series: [
+        {
+          name: 'Atténuation',
+          type: 'line',
+          yAxis: 0,
+          data: dampening,
+          tooltip: {
+            valueSuffix: ' dB',
+          },
+        },
+        {
+          name: 'Critère de Pluie',
+          yAxis: 1,
+          type: 'line',
+          data: rain,
+          tooltip: {
+            valueSuffix: ' %',
+          },
+          color: Highcharts.getOptions().colors[5],
+        },
+      ],
+    }));
 
-  // setChartOptions(() => ({ ...data }));
+  useEffect(() => {
+    setChartData(inputDampening, inputRain, inputTime);
+  }, []);
 
   return (
     <>
