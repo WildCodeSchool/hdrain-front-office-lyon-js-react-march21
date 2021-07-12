430856de7200dc9f81c337310c4372fe86bc33a1
Edouard Misset <edouardmisset@gmail.com>
2021-07-12 13:36:23 +0200

    added a call to the API for the Data assimilation & cleanup

--- src/screens/DataAssimilationPage.js
+++ src/screens/DataAssimilationPage.js
@@ -1,41 +1,32 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { LocationContext } from '../contexts/LocationContext';
-import asset from '../assets/sensor.png';
 import LocationDropDown from '../components/LocationDropDown';
 import AssimilationInfos from '../components/AssimilationInfos';
 import displayRelativeTimeFromNow from '../components/dateHelper';
+import API from '../APIClient';
 
 export default function DataAssimilationPage() {
-  const { selectedLocation, selectedLocationId } = useContext(LocationContext);
+  const { selectedLocation, selectedLocationId, experiment, setExperiment } =
+    useContext(LocationContext);
 
-  const [pathToLog] = useState(asset);
   const [showParams, setShowParams] = useState(false);
   const [locationParams, setLocationParams] = useState(['None']);
 
   const assimilationParams = [
     {
       location: 'Abidjan',
-      NX: 120,
-      NY: 300,
-      theta: 0.9,
     },
     {
       location: 'Antibes',
-      NX: 180,
-      NY: 192,
-      theta: 0.5,
     },
     {
       location: 'Toulouse',
-      NX: 150,
-      NY: 250,
-      theta: 0.8,
     },
   ];
 
   useEffect(() => {
-    if (selectedLocation === 'None') {
+    if (!selectedLocation) {
       setShowParams(false);
     } else {
       setLocationParams(
@@ -47,6 +38,16 @@ export default function DataAssimilationPage() {
     }
   }, [selectedLocation]);
 
+  useEffect(() => {
+    API.get(`/locations/${selectedLocationId}/experiments/`)
+      .then((res) => {
+        setExperiment(res.data);
+        console.log(res.data);
+      })
+      .catch(window.console.error);
+  }, [selectedLocationId]);
+
+  console.log(showParams);
   return (
     <>
       <h2>Data Assimilation</h2>
@@ -63,28 +64,19 @@ export default function DataAssimilationPage() {
           />
         </>
       ) : null}
-      <br />
       <Link
         className="download"
-        to={pathToLog}
+        to={experiment?.assimilationLog}
         target="_blank"
         download
-        style={
-          !selectedLocationId || selectedLocationId === 'None'
-            ? { pointerEvents: 'none' }
-            : null
-        }
+        style={!selectedLocationId ? { pointerEvents: 'none' } : null}
       >
         Download Data Assimilation Logs
       </Link>
       <Link
         to={`/locations/neuralNetwork?locationId=${selectedLocationId}`}
         className="link"
-        style={
-          !selectedLocationId || selectedLocationId === 'None'
-            ? { pointerEvents: 'none' }
-            : null
-        }
+        style={!selectedLocationId ? { pointerEvents: 'none' } : null}
       >
         Neural Network
       </Link>
