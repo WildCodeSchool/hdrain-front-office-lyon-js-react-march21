d61b437711f141cf42ba0ce5b6ce49d1f11698c4
Solene G <solene.gerard@gmail.com>
2021-07-15 00:05:27 +0200

    deleted info in popup

--- src/components/Map.js
+++ src/components/Map.js
@@ -40,12 +40,11 @@ export default function Map({
           <Marker key={name} icon={selectIcon(status)} position={[lat, lng]}>
             <Popup>
               <h4>
-                {name} {spotName.toUpperCase()}
+                {name} {spotName}
               </h4>
               <p>Sensor # {sensorNumber}</p>
               <p>Latitude : {lat}</p>
               <p>Longitude : {lng}</p>
-              <p>status : {status}</p>
             </Popup>
           </Marker>
         ))}
