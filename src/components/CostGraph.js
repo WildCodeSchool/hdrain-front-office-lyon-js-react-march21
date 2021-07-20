e1ee73dbf9645537986ed9f2f0b2bde52742344c
Edouard Misset <edouardmisset@gmail.com>
2021-07-20 19:30:15 +0200

    changed to the stored image rather than building the graph from data

--- src/components/CostGraph.js
+++ src/components/CostGraph.js
@@ -1,40 +1,26 @@
-import Highcharts from 'highcharts';
-import HighchartsReact from 'highcharts-react-official';
-import { useEffect, useState, useContext } from 'react';
+import { useContext } from 'react';
 import { LocationContext } from '../contexts/LocationContext';
-import graphOptions from '../utilities/graphHelper';
 
-export default function CostGraph({
-  inputJoNL = [1, 2, 3, 4, 5, 6, 7, 8],
-  inputJblin = [9, 8, 7, 6, 5, 4, 3, 2],
-  inputJlin = [19, 18, 17, 16, 15, 14, 13, 12],
-  inputR = [39, 38, 37, 36, 35, 34, 33, 32],
-}) {
-  const [chartOptions, setChartOptions] = useState(graphOptions);
-  const { locationName } = useContext(LocationContext);
-
-  const setChartData = (JoNL, Jblin, Jlin, r) =>
-    setChartOptions((previousOptions) => ({
-      ...previousOptions,
-      series: [
-        {
-          ...previousOptions.series[0],
-          data: JoNL,
-        },
-        { ...previousOptions.series[1], data: Jblin },
-        { ...previousOptions.series[2], data: Jlin },
-        { ...previousOptions.series[3], data: r },
-      ],
-    }));
-
-  useEffect(() => {
-    setChartData(inputJoNL, inputJblin, inputJlin, inputR);
-  }, []);
-
+const CostGraph = ({ width = 700, height = 530 }) => {
+  const { experiment, locationName } = useContext(LocationContext);
   return (
     <>
-      <h3>Costs Graph {locationName}</h3>
-      <HighchartsReact highcharts={Highcharts} options={chartOptions} />
+      <h3>Cost Graph {locationName}</h3>
+      {experiment?.costGraph ? (
+        <img
+          style={{
+            width: `${width}px`,
+            height: `${height}px`,
+          }}
+          className="costGraph"
+          src={experiment?.costGraph}
+          alt="costGraph"
+        />
+      ) : (
+        <p>No cost graph available for {experiment?.timestamp}</p>
+      )}
     </>
   );
-}
+};
+
+export default CostGraph;
